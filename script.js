0d9a7fceaad7240b618a82606e72a3cbbb379ea8
SamiyaMoh <121036367+SamiyaMoh@users.noreply.github.com>
2022-12-20 09:06:29 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,8 @@
+let userinput;
+var photos=[];
+
+$(".add").click(function() {
+  	userinput=$(".picture-url").val();
+	photos.push(userinput);
+	$(".gallery").append("<img src="+userinput+">");
+});
